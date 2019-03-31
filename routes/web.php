134674ee3d86db94b8187e02093d9693284fce46
Rio Firmansyah Eka Saputra <cyberlove80@gmail.com>
2019-03-31 22:45:22 +0700

    rubah route web dan install library uuid

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,6 @@ Route::group(['prefix' => 'register'], function () {
 //     return view('welcome');
 // });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
