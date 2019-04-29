5b13f708259f421f055ae86e8cf60ec7bc6a9ef4
rio firmansyah eka saputra <riofirmansyahekasaputra@gmail.com>
2019-04-29 14:39:06 +0700

    tambah dynamic dependent

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,9 +27,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         // Schema::defaultStringLength(191);
-        if(env('APP_ENV') !== 'local')
-        {
-            $url->forceSchema('https');
-        }
+        URL::forceScheme('https');
     }
 }
