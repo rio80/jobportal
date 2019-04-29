1fbb3a08cac057c37fdcda76f1970642da5ff8c9
rio firmansyah eka saputra <riofirmansyahekasaputra@gmail.com>
2019-04-29 14:29:33 +0700

    tambah dynamic dependent

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Schema::defaultStringLength(191);
+        // Schema::defaultStringLength(191);
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
+        }
     }
 }
